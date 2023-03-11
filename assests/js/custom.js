42ba2b0d54e58e070a62b707efe0e92f0e20051b
MOtawash Semeen Chowdhury <motawash15-14166@diu.edu.bd>
2023-03-11 19:40:50 +0600

    responsive nav & video

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -7,3 +7,21 @@ function myFunction() {
         document.querySelector(".navbar").classList.remove("shadow");
     }
 }
+
+
+$(".nav-mobile .nav-item1").click(function() {
+    $(".drop-show1").slideToggle();
+});
+$(".nav-mobile .nav-item2").click(function() {
+    $(".drop-show2").slideToggle();
+});
+$(".nav-mobile .nav-item3").click(function() {
+    $(".drop-show3").slideToggle();
+});
+$(".nav-mobile .nav-item4").click(function() {
+    $(".drop-show4").slideToggle();
+});
+
+$(window).resize(function() {
+    console.log($("body").width());
+});
